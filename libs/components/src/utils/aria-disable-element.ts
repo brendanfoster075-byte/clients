a88ced812c335f5b10cea0425ce2bc2011fef800
Bryan Cunningham <bryan.cunningham@me.com>
2025-08-07 12:33:04 -0400

    update comments

--- libs/components/src/utils/aria-disable-element.ts
+++ libs/components/src/utils/aria-disable-element.ts
@@ -3,14 +3,11 @@ import { effect } from "@angular/core";
 /**
  * a11y helper util used to `aria-disable` elements as opposed to using the HTML `disabled` attr.
  * - Removes HTML `disabled` attr and replaces it with `aria-disabled="true"`
- * - Captures click events and prevents them from propagating
  */
 export function ariaDisableElement(element: HTMLElement, isDisabled: boolean) {
   effect(() => {
     if (element.hasAttribute("disabled") || isDisabled) {
-      // Remove native disabled and set aria-disabled. Capture click event
       element.removeAttribute("disabled");
-
       element.setAttribute("aria-disabled", "true");
     }
   });
