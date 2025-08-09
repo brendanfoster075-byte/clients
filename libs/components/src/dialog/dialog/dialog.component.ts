e099ceea7448afc18146eee82fbcaed128edc42d
Mark Youssef <myoussef@bitwarden.com>
2025-08-08 17:33:22 -0700

    Apply feedback - Refactor to use signals - Refactor out responsive prop

--- libs/components/src/dialog/dialog/dialog.component.ts
+++ libs/components/src/dialog/dialog/dialog.component.ts
@@ -3,12 +3,12 @@ import { CdkScrollable } from "@angular/cdk/scrolling";
 import { CommonModule } from "@angular/common";
 import {
   Component,
-  HostBinding,
-  HostListener,
   inject,
   viewChild,
   input,
   booleanAttribute,
+  computed,
+  signal,
 } from "@angular/core";
 
 import { I18nPipe } from "@bitwarden/ui-common";
@@ -24,7 +24,9 @@ import { DialogTitleContainerDirective } from "../directives/dialog-title-contai
   selector: "bit-dialog",
   templateUrl: "./dialog.component.html",
   host: {
+    "[class]": "classes()",
     "(keydown.esc)": "handleEsc($event)",
+    "(animationend)": "onAnimationEnd()",
   },
   imports: [
     CommonModule,
@@ -70,16 +72,16 @@ export class DialogComponent {
    */
   readonly loading = input(false);
 
-  private animationCompleted = false;
+  private animationCompleted = signal(false);
 
-  @HostBinding("class") get classes() {
+  classes() {
     // `tw-max-h-[90vh]` is needed to prevent dialogs from overlapping the desktop header
     return [
       "tw-flex",
       "tw-flex-col",
       "tw-w-screen",
       // Prevent the animation from starting again when the viewport changes since it changes between breakpoints
-      ...(this.animationCompleted ? [] : this.animationClasses),
+      this.animationCompleted() ? [] : this.animationClasses(),
     ]
       .concat(
         this.width,
@@ -111,17 +113,16 @@ export class DialogComponent {
     }
   }
 
-  get animationClasses() {
+  protected animationClasses = computed(() => {
     switch (this.dialogSize()) {
       case "small":
         return ["tw-animate-slide-down"];
       default:
         return ["tw-animate-slide-up", "md:tw-animate-slide-down"];
     }
-  }
+  });
 
-  @HostListener("animationend")
   onAnimationEnd() {
-    this.animationCompleted = true;
+    this.animationCompleted.set(true);
   }
 }
