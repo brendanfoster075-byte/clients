cba7b6ed1ad977fcbe1043ac2a63ae59640fb01a
Bernd Schoolmann <mail@quexten.com>
2025-08-14 12:59:49 +0200

    Update comment

--- libs/common/src/platform/abstractions/key-generation.service.ts
+++ libs/common/src/platform/abstractions/key-generation.service.ts
@@ -1,1 +1,2 @@
+/** Temporary re-export. This should not be used for new imports */
 export { KeyGenerationService } from "../../key-management/crypto/key-generation/key-generation.service";
--- libs/common/src/platform/services/key-generation.service.ts
+++ libs/common/src/platform/services/key-generation.service.ts
@@ -1,1 +1,2 @@
+/** Temporary re-export. This should not be used for new imports */
 export { DefaultKeyGenerationService as KeyGenerationService } from "../../key-management/crypto/key-generation/default-key-generation.service";
